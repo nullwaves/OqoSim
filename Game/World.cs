5bbca0d7438538be19dfdc143c806d60f536b48c
null <evanb96@gmail.com>
2023-12-26 20:05:48 -0600

    Check z is valid before checking neighbor tiles

--- Game/World.cs
+++ Game/World.cs
@@ -16,8 +16,9 @@ namespace OqoSim.Game
 
         public Tile GetTileAtPos(int x, int y, int z) => Layers[z].Tiles[x, y];
         public Tile GetTileAbove(int x, int y, int z) => GetTileAtPos(x, y, z + 1);
-        public bool TileIsCovered(int x, int y, int z) => GetTileAbove(x, y, z).Type == TileType.Ground;
-        public bool TileIsSubmerged(int x, int y, int z) => GetTileAbove(x, y, z).Type == TileType.Water;
+        public bool TileIsCovered(int x, int y, int z) => ValidZ(z+1) ? GetTileAbove(x, y, z).Type == TileType.Ground : false;
+        public bool TileIsSubmerged(int x, int y, int z) => ValidZ(z+1) ? GetTileAbove(x, y, z).Type == TileType.Water : false;
+        public bool ValidZ(int z) => Layers.Keys.Contains(z);
 
         public List<IActor> GetActorsOnLayer(int z) => Actors.Where(a => a.Z == z).ToList();
     }
